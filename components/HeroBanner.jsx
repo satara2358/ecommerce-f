b5ff3c69771e0cbdeeb8f37bb55b6398190a0243
satara <25549072+satara2358@users.noreply.github.com>
2023-07-29 15:12:21 -0500

    tokens

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 import React from 'react';
+import { urlFor } from '../lib/client';
 
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
-      <p className='beats-solo'>Small Text</p>
-      <h3>Text Medio</h3>
-      <img src="" alt="headphones" className='hero-banner-image' />
+      <p className='beats-solo'>{heroBanner.smallText}</p>
+      <h3>{heroBanner.midText}</h3>
+      <h1>{heroBanner.largeText1}</h1>
+      <img src={urlFor(heroBanner.image)} alt="headphones" />
       <div>
         <Link href="/product/ID">
           <button type='button'>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
 import React from 'react'
 // import '../styles/globals.css';
-import { Product, FooterBanner, HeroBanner } from '../components'
+import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client.js';
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className='products-heading'>
         <h2>cualquier cosa</h2>
         <p>otra cosa</p>
       </div>
 
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map(
-          (product) => product)}
+        {products?.map(
+          (product) => product.name)}
       </div>
       <FooterBanner />
     </div>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return{
+    props: { products, bannerData }
+  }
+}
+
+export default Home;
