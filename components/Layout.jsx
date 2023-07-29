1820301e35c5d6c53138bcfbaad01d7af0bf1ea2
satara <25549072+satara2358@users.noreply.github.com>
2023-07-29 18:31:02 -0500

    Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,23 @@
+import Head from 'next/head'
 import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = () => {
   return (
-    <div>
+    <div className='layout'>
+      <Head>
+        <title>Tecnopolis Store</title>  
+      </Head> 
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        Vacio
+      </main>
+      <footer>
+        <Footer />
+      </footer>
       
     </div>
   )
